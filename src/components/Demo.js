470d475ad3f51b410a370d2bf5c8eec29d8fd747
SebG-prog <sebastien-gruet@orange.fr>
2020-06-23 16:10:31 +0200

    Multiple individual states

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Axios from 'axios'
 
 const Demo = () => {
+  const [name, setName] = useState('Bob')
+  const [loading, setLoading] = useState(false)
+  const [click, setClick] = useState(0)
+
+  const handleClick = () => {
+    setLoading(true)
+    Axios.get('https://randomuser.me/api/').then(res => {
+    setName(res.data.results[0].name.first)
+    setLoading(false)
+    setClick(click + 1)
+    })
+  }
+
   return (
-    <div>HI</div>
+    loading ? <div>Loading...</div> : 
+    <main>
+      <p>{name}</p>
+      <button onClick={handleClick}>Click ME</button>
+      <p>{click}</p>
+    </main>
   )
 }
 
