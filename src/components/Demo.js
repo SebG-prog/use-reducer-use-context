cf29fd3d2606c0b9ac956ab739b59168a109f21b
SebG-prog <sebastien-gruet@orange.fr>
2020-06-23 16:50:29 +0200

    Preparing the introduction of useContext. Creation of a child component 'GetName' (into the same file)

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -4,23 +4,28 @@ import Axios from 'axios'
 import { initialState, reducer } from './reducers/MonReducer'
 
 const Demo = () => {
-  const [{name, loading, click}, dispatch] = useReducer(reducer, initialState)
-
-  const handleClick = () => {
-    dispatch({ type : 'startGettingName'})
-    Axios.get('https://randomuser.me/api/').then(res => {
-    dispatch({ type: 'endGettingName', name: res.data.results[0].name.first})
-    })
-  }
-
+  const [{ name, loading, click }, dispatch] = useReducer(reducer, initialState)
   return (
-    loading ? <div>Loading...</div> : 
-    <main>
-      <p>{name}</p>
-      <button onClick={handleClick}>Click ME</button>
-      <p>{click}</p>
-    </main>
+    <GetName name={name} loading={loading} click={click} dispatch={dispatch} />
   )
 }
 
 export default Demo
+
+const GetName = ({name, loading, click, dispatch}) => {
+  const handleClick = () => {
+    dispatch({ type: 'startGettingName' })
+    Axios.get('https://randomuser.me/api/').then(res => {
+      dispatch({ type: 'endGettingName', name: res.data.results[0].name.first })
+    })
+  }
+
+  return (
+    loading ? <div>Loading...</div> :
+      <main>
+        <p>{name}</p>
+        <button onClick={handleClick}>Click ME</button>
+        <p>{click}</p>
+      </main>
+  )
+}
